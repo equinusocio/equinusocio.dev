c50ce04df4b69a3575fc5118f9a90c2a64577807
Mattia <astorino.mattia@gmail.com>
2021-04-29 13:39:43 +0200

    feat: add content to feed items

--- pages/api/rss.ts
+++ pages/api/rss.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import RSS from 'rss';
 import { getPosts } from '@/core/api/selectors';
+import marked from 'marked';
+import sanitizeHtml from 'sanitize-html';
 
 export default async function handler(_req: any, res: any) {
   const items: Record<string, any>[] = await getPosts();
@@ -8,8 +10,8 @@ export default async function handler(_req: any, res: any) {
   const feed = new RSS({
     title: 'Mattia Astorino - UX Engineer',
     description: 'I\'m Mattia Astorino, UX Engineer in Milan and member of Open Source Design.',
-    feed_url: `${process.env.NEXT_PUBLIC_DOMAIN}/api/rss`,
     site_url: process.env.NEXT_PUBLIC_DOMAIN,
+    feed_url: `${process.env.NEXT_PUBLIC_DOMAIN}/api/rss`,
     managingEditor: 'Mattia Astorino',
     webMaster: 'Mattia Astorino',
     copyright: `${new Date().getFullYear()} Mattia Astorino`,
@@ -20,18 +22,32 @@ export default async function handler(_req: any, res: any) {
   });
 
   items.forEach(({
-    title, description, publishDate, slug,
+    id, title, description, publishDate, slug, body,
   }) => {
     feed.item({
+      guid: id,
+      author: 'Mattia Astorino',
       title,
       description,
       url: `${process.env.NEXT_PUBLIC_DOMAIN}/blog/${slug}`,
       date: publishDate,
+      custom_elements: [
+        {
+          content: [
+            `${sanitizeHtml(marked(body))}`,
+            {
+              _attr: {
+                type: 'html',
+              },
+            },
+          ],
+        },
+      ],
     });
   });
 
-  const xml = feed.xml();
+  const rss = feed.xml({ indent: true });
   res.setHeader('Content-Type', 'text/xml');
-  res.write(xml);
+  res.write(rss);
   res.end();
 }
