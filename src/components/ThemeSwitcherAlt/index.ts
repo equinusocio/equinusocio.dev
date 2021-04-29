16444f6e40b557f5a46459f1424592195ff469dc
Mattia <astorino.mattia@gmail.com>
2021-04-29 15:59:50 +0200

    chore: remove old theme switcher

--- src/components/ThemeSwitcherAlt/index.ts
+++
@@ -1,4 +0,0 @@
-export { ThemeSwitcher, IThemeSwitcherProps } from './ThemeSwitcher';
-export { ThemeProvider, ThemeContext } from './ThemeProvider';
-export { useThemeContext } from './useThemeContext';
-export { useThemeCSS } from './useThemeCSS';
