881daf576ef114bb1580e00e12bdbd27666686e8
Mattia <astorino.mattia@gmail.com>
2021-05-06 17:36:09 +0200

    chore: update pills cloud

--- src/components/Pill/pill.tsx
+++ src/components/Pill/pill.tsx
@@ -2,29 +2,23 @@ import clsx from 'clsx';
 import React, {
   HTMLAttributes,
   ReactNode,
-  Children,
-  cloneElement,
 } from 'react';
 import style from './pill.module.css';
 
 interface IPillProps extends HTMLAttributes<HTMLElement> {
   icon?: ReactNode;
+  color?: 'purple' | 'cyan' | 'blue' | 'orange' | 'violet' | 'lime' | 'green' | 'yellow' | 'black' | 'pink' | 'grey';
 }
 
 export const Pill = ({
   children,
   className,
   icon,
+  color = 'grey',
   ...props
 }: IPillProps) => (
-  <span className={clsx(style.Pill, className)} {...props}>
-    {Children.map(icon, (child: any) => cloneElement(
-      child,
-      {
-        'aria-hidden': 'true',
-        role: 'presentation',
-      },
-    ))}
+  <span data-pill-color={color} className={clsx(style.Pill, className)} {...props}>
+    {icon}
     {children}
   </span>
 );
--- src/components/PillsCloud/pills-cloud.tsx
+++ src/components/PillsCloud/pills-cloud.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 import React, { HTMLAttributes } from 'react';
 import { Pill } from '@/components/Pill';
 import {
-  Accessibility, Design, Figma, Geometry, Javascript, Nextjs, PenTool, WindowDev, ReactIcon, Css,
+  Accessibility, Design, Figma, Geometry, Javascript, Nextjs, PenTool, WindowDev, ReactIcon, Css, Magic,
 } from '@/components/Icons';
 import { Stack } from '@/components/Stack';
 import style from './pills-cloud.module.css';
@@ -12,15 +12,38 @@ export const PillsCloud = ({
   ...props
 }: HTMLAttributes<HTMLElement>) => (
   <Stack as="ul" className={clsx(style.PillsCloud, className)} {...props}>
-    <li className={style.Item} style={{ '--top': '140px', '--left': '10%' }}><Pill icon={<PenTool />}>Design Systems</Pill></li>
-    <li className={style.Item} style={{ '--top': '170px', '--left': '60%' }}><Pill icon={<WindowDev />}>HTML</Pill></li>
-    <li className={style.Item} style={{ '--top': '235px', '--left': '30%' }}><Pill icon={<Javascript />}>JS</Pill></li>
-    <li className={style.Item} style={{ '--top': '300px', '--left': '80%' }}><Pill icon={<Nextjs />}>Next.js</Pill></li>
-    <li className={style.Item} style={{ '--top': '320px', '--left': '5%' }}><Pill icon={<Figma />}>Figma</Pill></li>
-    <li className={style.Item} style={{ '--top': '390px', '--left': '40%' }}><Pill icon={<Geometry />}>Components libraries</Pill></li>
-    <li className={style.Item} style={{ '--top': '290px', '--left': '53%' }}><Pill icon={<Accessibility />}>A11y</Pill></li>
-    <li className={style.Item} style={{ '--top': '80px', '--left': '75%' }}><Pill icon={<Design />}>Design</Pill></li>
-    <li className={style.Item} style={{ '--top': '-20px', '--left': '60%' }}><Pill icon={<ReactIcon />}>React</Pill></li>
-    <li className={style.Item} style={{ '--top': '30px', '--left': '30%' }}><Pill icon={<Css />}>CSS</Pill></li>
+    <li className={style.Item} style={{ '--top': '140px', '--left': '10%' }}>
+      <Pill color="lime" icon={<PenTool aria-hidden="true" role="presentation" />}>Design Systems</Pill>
+    </li>
+    <li className={style.Item} style={{ '--top': '170px', '--left': '60%' }}>
+      <Pill color="green" icon={<WindowDev aria-hidden="true" role="presentation" />}>HTML</Pill>
+    </li>
+    <li className={style.Item} style={{ '--top': '235px', '--left': '30%' }}>
+      <Pill color="yellow" icon={<Javascript aria-hidden="true" role="presentation" />}>JS</Pill>
+    </li>
+    <li className={style.Item} style={{ '--top': '300px', '--left': '80%' }}>
+      <Pill color="black" icon={<Nextjs aria-hidden="true" role="presentation" />}>Next.js</Pill>
+    </li>
+    <li className={style.Item} style={{ '--top': '320px', '--left': '5%' }}>
+      <Pill color="pink" icon={<Figma aria-hidden="true" role="presentation" />}>Figma</Pill>
+    </li>
+    <li className={style.Item} style={{ '--top': '390px', '--left': '40%' }}>
+      <Pill color="orange" icon={<Geometry aria-hidden="true" role="presentation" />}>Components libraries</Pill>
+    </li>
+    <li className={style.Item} style={{ '--top': '290px', '--left': '53%' }}>
+      <Pill color="grey" icon={<Accessibility aria-hidden="true" role="presentation" />}>A11y</Pill>
+    </li>
+    <li className={style.Item} style={{ '--top': '80px', '--left': '75%' }}>
+      <Pill color="violet" icon={<Design aria-hidden="true" role="presentation" />}>Design</Pill>
+    </li>
+    <li className={style.Item} style={{ '--top': '-20px', '--left': '60%' }}>
+      <Pill color="blue" icon={<ReactIcon aria-hidden="true" role="presentation" />}>React</Pill>
+    </li>
+    <li className={style.Item} style={{ '--top': '30px', '--left': '30%' }}>
+      <Pill color="cyan" icon={<Css aria-hidden="true" role="presentation" />}>CSS</Pill>
+    </li>
+    <li className={style.Item} style={{ '--top': '-60px', '--left': '10%' }}>
+      <Pill color="purple" icon={<Magic aria-hidden="true" role="presentation" />}>Interactions</Pill>
+    </li>
   </Stack>
 );
