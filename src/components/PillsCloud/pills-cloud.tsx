cd8cdc8d30ec88fc7bd7e7e64983f622727e9342
Mattia <astorino.mattia@gmail.com>
2021-05-06 18:37:59 +0200

    chore: code formatting

--- src/components/PillsCloud/pills-cloud.tsx
+++ src/components/PillsCloud/pills-cloud.tsx
@@ -49,85 +49,138 @@ export const PillsCloud = ({
       ref={myRef}
       className={clsx(style.PillsCloud, className)}
     >
-      {/* <Stack as="ul" {...props}> */}
       <motion.li
         className={style.Item}
         style={{ '--top': '140px', '--left': '10%' } as CSSProperties}
         variants={item}
       >
-        <Pill color="lime" icon={<PenTool aria-hidden="true" role="presentation" />}>Design Systems</Pill>
+        <Pill
+          color="lime"
+          icon={<PenTool aria-hidden="true" role="presentation" />}
+        >
+          Design Systems
+        </Pill>
       </motion.li>
       <motion.li
         className={style.Item}
         style={{ '--top': '170px', '--left': '60%' } as CSSProperties}
         variants={item}
       >
-        <Pill color="green" icon={<WindowDev aria-hidden="true" role="presentation" />}>HTML</Pill>
+        <Pill
+          color="green"
+          icon={<WindowDev aria-hidden="true" role="presentation" />}
+        >
+          HTML
+        </Pill>
       </motion.li>
       <motion.li
         className={style.Item}
         style={{ '--top': '235px', '--left': '30%' } as CSSProperties}
         variants={item}
       >
-        <Pill color="yellow" icon={<Javascript aria-hidden="true" role="presentation" />}>JS</Pill>
+        <Pill
+          color="yellow"
+          icon={<Javascript aria-hidden="true" role="presentation" />}
+        >
+          JS
+        </Pill>
       </motion.li>
       <motion.li
         className={style.Item}
         style={{ '--top': '300px', '--left': '80%' } as CSSProperties}
         variants={item}
       >
-        <Pill color="black" icon={<Nextjs aria-hidden="true" role="presentation" />}>Next.js</Pill>
+        <Pill
+          color="black"
+          icon={<Nextjs aria-hidden="true" role="presentation" />}
+        >
+          Next.js
+        </Pill>
       </motion.li>
       <motion.li
         className={style.Item}
         style={{ '--top': '320px', '--left': '5%' } as CSSProperties}
         variants={item}
       >
-        <Pill color="pink" icon={<Figma aria-hidden="true" role="presentation" />}>Figma</Pill>
+        <Pill
+          color="pink"
+          icon={<Figma aria-hidden="true" role="presentation" />}
+        >
+          Figma
+        </Pill>
       </motion.li>
       <motion.li
         className={style.Item}
         style={{ '--top': '390px', '--left': '40%' } as CSSProperties}
         variants={item}
       >
-        <Pill color="orange" icon={<Geometry aria-hidden="true" role="presentation" />}>Components libraries</Pill>
+        <Pill
+          color="orange"
+          icon={<Geometry aria-hidden="true" role="presentation" />}
+        >
+          Components libraries
+        </Pill>
       </motion.li>
       <motion.li
         className={style.Item}
         style={{ '--top': '290px', '--left': '53%' } as CSSProperties}
         variants={item}
       >
-        <Pill color="grey" icon={<Accessibility aria-hidden="true" role="presentation" />}>A11y</Pill>
+        <Pill
+          color="grey"
+          icon={<Accessibility aria-hidden="true" role="presentation" />}
+        >
+          A11y
+        </Pill>
       </motion.li>
       <motion.li
         className={style.Item}
         style={{ '--top': '80px', '--left': '75%' } as CSSProperties}
         variants={item}
       >
-        <Pill color="violet" icon={<Design aria-hidden="true" role="presentation" />}>Design</Pill>
+        <Pill
+          color="violet"
+          icon={<Design aria-hidden="true" role="presentation" />}
+        >
+          Design
+        </Pill>
       </motion.li>
       <motion.li
         className={style.Item}
         style={{ '--top': '-20px', '--left': '60%' } as CSSProperties}
         variants={item}
       >
-        <Pill color="blue" icon={<ReactIcon aria-hidden="true" role="presentation" />}>React</Pill>
+        <Pill
+          color="blue"
+          icon={<ReactIcon aria-hidden="true" role="presentation" />}
+        >
+          React
+        </Pill>
       </motion.li>
       <motion.li
         className={style.Item}
         style={{ '--top': '30px', '--left': '30%' } as CSSProperties}
         variants={item}
       >
-        <Pill color="cyan" icon={<Css aria-hidden="true" role="presentation" />}>CSS</Pill>
+        <Pill
+          color="cyan"
+          icon={<Css aria-hidden="true" role="presentation" />}
+        >
+          CSS
+        </Pill>
       </motion.li>
       <motion.li
         className={style.Item}
         style={{ '--top': '-60px', '--left': '10%' } as CSSProperties}
         variants={item}
       >
-        <Pill color="purple" icon={<Magic aria-hidden="true" role="presentation" />}>Interactions</Pill>
+        <Pill
+          color="purple"
+          icon={<Magic aria-hidden="true" role="presentation" />}
+        >
+          Interactions
+        </Pill>
       </motion.li>
-      {/* </Stack> */}
     </motion.ul>
   );
 };
