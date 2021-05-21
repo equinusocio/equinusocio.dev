caeada091ba04c2193e0296f8a36703ec87bd257
Mattia <astorino.mattia@gmail.com>
2021-05-21 08:27:08 +0200

    chore: clean css

--- src/components/TextReveal/text-reveal.tsx
+++ src/components/TextReveal/text-reveal.tsx
@@ -1,18 +1,12 @@
-import React, { CSSProperties, HTMLAttributes } from 'react';
+import React, { HTMLAttributes } from 'react';
 import { useIntersection } from 'react-use';
 import styles from './text-reveal.module.css';
-
-interface ITextRevealProps extends HTMLAttributes<HTMLSpanElement> {
-  color?: string;
-}
 
 export const TextReveal = ({
   children,
   className,
-  color,
-  style,
   ...props
-}: ITextRevealProps) => {
+}: HTMLAttributes<HTMLSpanElement>) => {
   const componentRef = React.useRef(null);
   const intersection = useIntersection(componentRef, {
     root: null,
@@ -20,16 +14,11 @@ export const TextReveal = ({
     threshold: 0,
   });
 
-  const dynamicStyle: CSSProperties = {
-    '--c': color,
-  };
-
   return (
     <span
       ref={componentRef}
       className={styles.TextReveal}
       data-text-reveal-is-running={intersection?.isIntersecting}
-      style={{ ...style, ...dynamicStyle }}
       {...props}
     >
       {children}
